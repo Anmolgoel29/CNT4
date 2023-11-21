067d1005ad0104873ff3111a81827834f11c6098
Anmolgoel29 <88969348+Anmolgoel29@users.noreply.github.com>
2023-11-21 13:02:43 +0530

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -3,7 +3,7 @@ let btn=document.querySelectorAll(".menu p");
 Array.from(btn).forEach((elem)=>{
     elem.addEventListener("click",()=>{
         // console.log(elem.innerHTML.trim());
-        if(elem.innerHTML.trim()=='PLAY VS CPU'){
+        if(elem.innerHTML.trim()=='Start Game'){
             window.location.href="cpu.html";
         }
     });
